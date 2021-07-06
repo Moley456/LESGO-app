95b40204683c2df62c7f5c99a493fa9a131f2115
ryyant <ryyant@hotmail.com>
2021-07-06 16:49:15 +0800

    Chat feature #2

--- src/screens/HomeScreens/ChatScreen.js
+++ src/screens/HomeScreens/ChatScreen.js
@@ -1,29 +1,46 @@
-import React, { useState, useCallback, useEffect } from 'react'
-import { GiftedChat } from 'react-native-gifted-chat'
-
-import { SafeAreaView, StyleSheet, Text, StatusBar, TouchableOpacity, Image } from 'react-native';
-import HideKeyboard from '../../components/HideKeyboard';
-import { Ionicons } from '@expo/vector-icons';
-import firebase from 'firebase';
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat, Bubble, Day } from "react-native-gifted-chat";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import HideKeyboard from "../../components/HideKeyboard";
+import { Ionicons } from "@expo/vector-icons";
+import firebase from "firebase";
 import * as Auth from "../../../api/auth";
 
-
-const db = firebase.database();
-
 export default ({ navigation, route }) => {
-    const [messages, setMessages] = useState([]);
-    const roomId = route.params.params.key;
+  const db = firebase.database();
+  const [messages, setMessages] = useState([]);
+  const roomId = route.params.params.key;
 
-    const logData = (newMessage) => {
-        const timeInString = newMessage[0].createdAt.toString();
-        newMessage[0].createdAt = timeInString;
-        db.ref("app/chats/" + roomId).update({
-            [newMessage[0]._id]: newMessage[0]
-        })
+  const logData = (newMessage) => {
+    const timeInString = newMessage[0].createdAt.toString();
+    newMessage[0].createdAt = timeInString;
+    db.ref("app/chats/" + roomId).update({
+      [messages.length + 1]: newMessage[0],
+    });
+  };
 
-    }
+  useEffect(() => {
+    setMessages([]);
+    const sub = db
+      .ref("app/chats/" + roomId)
+      .on("child_added", (snapshot, prevChildKey) => {
+        setMessages((messages) => [
+          { ...snapshot.val(), key: snapshot.val()._id },
+          ...messages,
+        ]);
+      });
 
- /*    useEffect(() => {
+    return () => sub;
+  }, []);
+
+  /*    useEffect(() => {
       setMessages([
         {
           _id: 1,
@@ -38,37 +55,70 @@ export default ({ navigation, route }) => {
       ])
     }, []) */
 
-    useEffect(() => {
-        setMessages([])
-        db.ref('app/chats/' + roomId).once('value', (snapshot) => {
-            snapshot.forEach((data) => {
-                 setMessages((messages) => [...messages, {...data.val(), key: data.val()._id}])
-            })
-        })
-    }, [])
-
-    const onSend = useCallback((messages = []) => {
+  /*     const onSend = useCallback((messages = []) => {
       setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-      logData(messages)
-    }, [])
-    
-    
+    }, []) */
+
   return (
     <HideKeyboard>
       <SafeAreaView style={styles.container}>
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
           <Ionicons name="chevron-back" size={32} />
         </TouchableOpacity>
         <Text style={styles.header}>Chat</Text>
 
         <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: Auth.getCurrentUserName(),
-      }}
-    />
-
+          messages={messages}
+          onSend={(messages) => {
+            logData(messages);
+          }}
+          user={{
+            _id: Auth.getCurrentUserName(),
+            name: Auth.getCurrentUserName(),
+            avatar: "https://placeimg.com/140/140/any",
+          }}
+          renderUsernameOnMessage={true}
+          showUserAvatar={true}
+          onPressAvatar={() => {}}
+          textProps={{ style: { color: "black" } }}
+          timeTextStyle={{ left: { color: "grey" }, right: { color: "grey" } }}
+          renderChatEmpty={() => {
+            return (
+              <View style={styles.emptyChat}>
+                <Text style={styles.subHeader}>Start chatting!</Text>
+              </View>
+            );
+          }}
+          renderDay={(props) => {
+            return (
+              <Day
+                {...props}
+                textStyle={{ color: "black", fontFamily: "Montserrat_700Bold" }}
+              />
+            );
+          }}
+          renderBubble={(props) => {
+            return (
+              <Bubble
+                {...props}
+                wrapperStyle={{
+                  left: {
+                    backgroundColor: "#b8d5cd",
+                  },
+                  right: {
+                    backgroundColor: "#F8F5F1",
+                  },
+                }}
+                usernameStyle={{
+                  color: "grey",
+                }}
+              />
+            );
+          }}
+        />
       </SafeAreaView>
     </HideKeyboard>
   );
@@ -77,32 +127,37 @@ export default ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#5AA397',
+    backgroundColor: "#5AA397",
     paddingTop: StatusBar.currentHeight ? StatusBar.currentHeight : 0,
     paddingBottom: 70,
   },
 
   backButton: {
-    position: 'absolute',
-    alignSelf: 'flex-start',
-    top: '7%',
-    left: '3%',
+    position: "absolute",
+    alignSelf: "flex-start",
+    top: "7%",
+    left: "3%",
   },
 
   header: {
     fontSize: 55,
-    color: '#F8F5F1',
-    fontFamily: 'Montserrat_700Bold',
+    color: "#F8F5F1",
+    fontFamily: "Montserrat_700Bold",
     marginTop: 50,
-    marginLeft: '10%'
+    marginLeft: "10%",
   },
 
   subHeader: {
-    fontSize: 25,
-    fontFamily: 'Montserrat_700Bold',
-    marginVertical: 10,
-    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: "Roboto_400Regular",
+    paddingBottom: "10%",
   },
 
-
+  emptyChat: {
+    transform: [{ scaleY: -1 }],
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
