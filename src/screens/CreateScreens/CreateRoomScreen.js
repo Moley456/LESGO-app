b2092828bedbab9efeed86c39fc2a7ceedef6a34
ryyant <ryyant@hotmail.com>
2021-07-07 16:28:09 +0800

    date time update

--- src/screens/CreateScreens/CreateRoomScreen.js
+++ src/screens/CreateScreens/CreateRoomScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -12,53 +12,41 @@ import {
   Button,
   FlatList,
   ActivityIndicator,
-} from 'react-native';
-import { CommonActions } from '@react-navigation/native';
-import { getCurrentUserId, getCurrentUserName } from '../../../api/auth';
-import * as Friends from '../../../api/friends';
-import HideKeyboard from '../../components/HideKeyboard';
-import { FontAwesome } from '@expo/vector-icons';
-import firebase from 'firebase';
-import { Searchbar } from 'react-native-paper';
-import * as Auth from '../../../api/auth';
-import * as Maths from '../../../api/maths';
-import DateTimePicker from '@react-native-community/datetimepicker';
+} from "react-native";
+import { CommonActions } from "@react-navigation/native";
+import { getCurrentUserId, getCurrentUserName } from "../../../api/auth";
+import * as Friends from "../../../api/friends";
+import HideKeyboard from "../../components/HideKeyboard";
+import { FontAwesome } from "@expo/vector-icons";
+import firebase from "firebase";
+import { Searchbar } from "react-native-paper";
+import * as Auth from "../../../api/auth";
+import * as Maths from "../../../api/maths";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default ({ navigation }) => {
   const db = firebase.database();
   const username = getCurrentUserName();
-  const [roomName, setRoomName] = React.useState('');
+  const [roomName, setRoomName] = React.useState("");
   const [date, setDate] = React.useState(Maths.getCurrentTime());
   const [time, setTime] = React.useState(Maths.getCurrentTime());
-  const [limit, setLimit] = React.useState('');
+  const [limit, setLimit] = React.useState("");
   const [modalVisible, setModalVisible] = useState(false);
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [results, setResults] = useState([]);
   const [participants, setParticipants] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
   const [baseResults, setBaseResults] = useState([]);
-  const [showTimePicker, setShowTimePicker] = useState(false);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-
-  // event prop is required!!
-  const onDateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShowDatePicker(false);
-    setDate(currentDate);
-  };
-
-  // event prop is required!!
-  const onTimeChange = (event, selectedTime) => {
-    const currentTime = selectedTime || time;
-    setShowTimePicker(false);
-    setTime(currentTime);
-    console.log(currentTime);
-  };
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
 
   const handleSearch = (text) => {
     setSearchInput(text);
-    const filtered = results.filter((item) => item.username.includes(text.toLowerCase()));
+    const filtered = results.filter((item) =>
+      item.username.includes(text.toLowerCase())
+    );
     setFilteredResults(filtered);
   };
 
@@ -68,75 +56,71 @@ export default ({ navigation }) => {
         navigation.dispatch(
           CommonActions.reset({
             index: 0,
-            routes: [{ name: 'Login' }],
+            routes: [{ name: "Login" }],
           })
         ),
       console.error
     );
   };
 
-  const handleDatePress = () => {
-    setShowDatePicker(true);
-  };
-
-  const handleTimePress = () => {
-    setShowTimePicker(true);
-  };
-
   const invite = () => {
-    if (roomName === '' || date === '' || time === '' || limit === '') {
-      Alert.alert('Please fill in every field!');
+    if (roomName === "" || date === "" || time === "" || limit === "") {
+      Alert.alert("Please fill in every field!");
     } else if (participants.length === 0) {
-      Alert.alert('No friends added!');
+      Alert.alert("No friends added!");
     } else {
-      const pushedPostRef = db.ref('app/rooms/').push({ details: [1] });
+      const pushedPostRef = db.ref("app/rooms/").push({ details: [1] });
       const postId = pushedPostRef.getKey();
-      db.ref('app/rooms/' + postId + '/details').set({
+      db.ref("app/rooms/" + postId + "/details").set({
         roomName: roomName,
         date: date.toDateString(),
-        time: time.toTimeString().split(' ')[0],
+        time: time.toTimeString().split(" ")[0],
         timeCreated: Maths.getCurrentTime().toString(),
         timeEnded: Maths.getTimeAfter(limit).toString(),
         creator: username,
-        activity: '',
+        activity: "",
       });
 
       participants.forEach((item) => {
-        db.ref('app/rooms/' + postId + '/participants').update({
+        db.ref("app/rooms/" + postId + "/participants").update({
           [item]: false,
         });
       });
 
-      db.ref('app/rooms/' + postId + '/participants').update({
+      db.ref("app/rooms/" + postId + "/participants").update({
         [username]: false,
       });
 
-      db.ref('app/participants/' + username).update({
+      db.ref("app/participants/" + username).update({
         [postId]: false,
       });
 
       for (var i = 0; i < participants.length; i++) {
-        db.ref('app/participants/' + participants[i]).update({
+        db.ref("app/participants/" + participants[i]).update({
           [postId]: false,
         });
       }
 
-      navigation.navigate('InviteSent');
+      navigation.navigate("InviteSent");
     }
   };
 
   useEffect(() => {
     if (participants[0] !== null) {
-      setFilteredResults((old) => old.filter((item) => !participants.includes(item.username)));
-      setResults((old) => old.filter((item) => !participants.includes(item.username)));
+      setFilteredResults((old) =>
+        old.filter((item) => !participants.includes(item.username))
+      );
+      setResults((old) =>
+        old.filter((item) => !participants.includes(item.username))
+      );
     }
   }, [participants]);
 
   useEffect(() => {
-    db.ref('app/friends/' + getCurrentUserId())
+    db.ref("app/friends/" + getCurrentUserId())
       .orderByValue()
       .equalTo(true)
-      .on('value', (snapshot) => {
+      .on("value", (snapshot) => {
         setFilteredResults([]);
         setResults([]);
         snapshot.forEach((data) => {
@@ -151,10 +135,10 @@ export default ({ navigation }) => {
       });
 
     return db
-      .ref('app/friends/' + getCurrentUserId())
+      .ref("app/friends/" + getCurrentUserId())
       .orderByValue()
       .equalTo(true)
-      .off('value', (snapshot) => {
+      .off("value", (snapshot) => {
         setFilteredResults([]);
         setResults([]);
         snapshot.forEach((data) => {
@@ -182,60 +166,76 @@ export default ({ navigation }) => {
 
         <View style={styles.row}>
           <Text style={styles.tabBoldText}>Name</Text>
-          <TextInput style={styles.input} onChangeText={setRoomName} value={roomName} keyboardType={'default'} maxLength={10}></TextInput>
+          <TextInput
+            style={styles.input}
+            onChangeText={setRoomName}
+            value={roomName}
+            keyboardType={"default"}
+            maxLength={10}
+          ></TextInput>
         </View>
 
         {/* DatePicker */}
         <View style={styles.row}>
           <Text style={styles.tabBoldText}>Date</Text>
-          <TouchableOpacity style={styles.input} onPress={handleDatePress}>
-            <Text style={styles.dateAndTimeText}>{date.toString().slice(0, 15)}</Text>
+          <TouchableOpacity
+            style={styles.input}
+            onPress={() => setDatePickerVisibility(true)}
+          >
+            <Text style={styles.dateAndTimeText}>
+              {date.toString().slice(0, 15)}
+            </Text>
+            <View>
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={() => setDatePickerVisibility(false)}
+                onCancel={() => setDatePickerVisibility(false)}
+                onChange={(date) => setDate(date)}
+                minimumDate={Maths.getCurrentTime()}
+              />
+            </View>
           </TouchableOpacity>
-          {showDatePicker && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode={'date'}
-              is24Hour={true}
-              display="default"
-              onChange={onDateChange}
-              minimumDate={Maths.getCurrentTime()}
-            />
-          )}
         </View>
 
         {/* TimePicker */}
         <View style={styles.row}>
           <Text style={styles.tabBoldText}>Time</Text>
-          <TouchableOpacity style={styles.input} onPress={handleTimePress}>
-            <Text style={styles.dateAndTimeText}>{time.toString().slice(15, 21)}</Text>
+          <TouchableOpacity
+            style={styles.input}
+            onPress={() => setTimePickerVisibility(true)}
+          >
+            <Text style={styles.dateAndTimeText}>
+              {time.toString().slice(15, 21)}
+            </Text>
+            <View>
+              <DateTimePickerModal
+                isVisible={isTimePickerVisible}
+                mode="time"
+                onConfirm={() => setTimePickerVisibility(false)}
+                onCancel={() => setTimePickerVisibility(false)}
+                onChange={(time) => setTime(time)}
+                minimumDate={Maths.getCurrentTime()}
+              />
+            </View>
           </TouchableOpacity>
-          {showTimePicker && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={time}
-              mode={'time'}
-              is24Hour={true}
-              display="default"
-              onChange={onTimeChange}
-              minimumDate={Maths.getCurrentTime()}
-            />
-          )}
         </View>
 
         {/* Time Limit */}
         <View style={styles.row}>
           <Text style={styles.tabBoldText}>Time Limit</Text>
           <TextInput
-            style={[styles.input, { width: '40%' }]}
+            style={[styles.input, { width: "40%" }]}
             onChangeText={setLimit}
             value={limit}
-            keyboardType={'number-pad'}
+            keyboardType={"number-pad"}
             maxLength={1}
           ></TextInput>
         </View>
 
-        <Text style={styles.tabText}>*Time for participants to indicate preferences!</Text>
+        <Text style={styles.tabText}>
+          *Time for participants to indicate preferences!
+        </Text>
 
         <Modal
           animationType="slide"
@@ -251,7 +251,7 @@ export default ({ navigation }) => {
 
               <View>
                 <Searchbar
-                  theme={{ colors: { primary: '#5AA397' } }}
+                  theme={{ colors: { primary: "#5AA397" } }}
                   style={styles.search}
                   placeholder="search"
                   inputStyle={styles.inputStyle}
@@ -262,7 +262,7 @@ export default ({ navigation }) => {
                 />
               </View>
 
-              <View style={{ width: '100%' }}>
+              <View style={{ width: "100%" }}>
                 <FlatList
                   keyExtractor={(item) => item.email}
                   data={filteredResults}
@@ -272,19 +272,20 @@ export default ({ navigation }) => {
                       style={[
                         styles.listItem,
                         {
-                          backgroundColor: index % 2 === 0 ? '#D8D4CF' : '#E3E0DB',
+                          backgroundColor:
+                            index % 2 === 0 ? "#D8D4CF" : "#E3E0DB",
                         },
                       ]}
                     >
                       <FontAwesome name="user-circle" size={20} />
                       <Text style={styles.names}>
-                        {item.username} {'\n'} @tag
+                        {item.username} {"\n"} @tag
                       </Text>
                       <TouchableOpacity
                         style={styles.addButton}
                         onPress={() => {
                           setParticipants((old) => [...old, item.username]);
-                          Alert.alert('Invited ' + item.username + '!');
+                          Alert.alert("Invited " + item.username + "!");
                         }}
                       >
                         <Text style={styles.inputStyle}>Add</Text>
@@ -320,7 +321,10 @@ export default ({ navigation }) => {
           </HideKeyboard>
         </Modal>
 
-        <TouchableOpacity style={styles.addFriends} onPress={() => setModalVisible(true)}>
+        <TouchableOpacity
+          style={styles.addFriends}
+          onPress={() => setModalVisible(true)}
+        >
           <Text style={styles.addFriendsText}>ADD FRIENDS</Text>
         </TouchableOpacity>
 
@@ -340,114 +344,114 @@ export default ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: '#5AA397',
+    width: "100%",
+    backgroundColor: "#5AA397",
     paddingTop: StatusBar.currentHeight ? StatusBar.currentHeight : 0,
     paddingBottom: 70,
-    alignItems: 'center',
+    alignItems: "center",
   },
 
   logoutButton: {
-    alignSelf: 'flex-end',
-    height: '4%',
+    alignSelf: "flex-end",
+    height: "4%",
     marginTop: 20,
     marginRight: 10,
   },
 
   logoutText: {
-    fontFamily: 'Roboto_400Regular',
+    fontFamily: "Roboto_400Regular",
     fontSize: 20,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
 
   headerText: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginHorizontal: 35,
-    marginTop: '15%',
-    marginBottom: '8%',
+    marginTop: "15%",
+    marginBottom: "8%",
     fontSize: 65,
-    color: '#F8F5F1',
-    fontFamily: 'Montserrat_700Bold',
+    color: "#F8F5F1",
+    fontFamily: "Montserrat_700Bold",
   },
 
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
     marginBottom: 15,
   },
 
   input: {
-    backgroundColor: '#F8F5F1',
-    fontFamily: 'Montserrat_700Bold',
-    textAlign: 'center',
+    backgroundColor: "#F8F5F1",
+    fontFamily: "Montserrat_700Bold",
+    textAlign: "center",
     fontSize: 20,
-    width: '60%',
-    height: '100%',
+    width: "60%",
+    height: "100%",
     borderRadius: 20,
     marginRight: 30,
   },
 
   dateAndTimeText: {
-    textAlign: 'center',
+    textAlign: "center",
     paddingTop: 2,
     fontSize: 20,
-    fontFamily: 'Montserrat_700Bold',
+    fontFamily: "Montserrat_700Bold",
   },
 
   tabBoldText: {
     fontSize: 25,
-    fontFamily: 'Montserrat_700Bold',
+    fontFamily: "Montserrat_700Bold",
     marginLeft: 30,
   },
 
   addFriends: {
-    backgroundColor: '#F8F5F1',
+    backgroundColor: "#F8F5F1",
     borderRadius: 5,
     marginTop: 25,
     marginHorizontal: 15,
-    width: '85%',
-    height: '6.5%',
-    justifyContent: 'center',
+    width: "85%",
+    height: "6.5%",
+    justifyContent: "center",
     borderWidth: 2,
-    borderColor: 'black',
+    borderColor: "black",
   },
 
   addFriendsText: {
     fontSize: 18,
-    fontFamily: 'Montserrat_700Bold',
-    alignSelf: 'center',
+    fontFamily: "Montserrat_700Bold",
+    alignSelf: "center",
   },
 
   invite: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     borderRadius: 5,
     margin: 15,
-    width: '85%',
-    height: '6.5%',
-    justifyContent: 'center',
+    width: "85%",
+    height: "6.5%",
+    justifyContent: "center",
   },
 
   inviteText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontFamily: 'Montserrat_700Bold',
-    alignSelf: 'center',
+    fontFamily: "Montserrat_700Bold",
+    alignSelf: "center",
   },
 
   modalView: {
-    alignSelf: 'center',
-    alignItems: 'center',
-    width: '75%',
-    height: '45%',
-    marginTop: '40%',
-    marginBottom: '20%',
-    backgroundColor: '#F8F5F1',
+    alignSelf: "center",
+    alignItems: "center",
+    width: "75%",
+    height: "45%",
+    marginTop: "40%",
+    marginBottom: "20%",
+    backgroundColor: "#F8F5F1",
     borderRadius: 20,
-    paddingTop: '10%',
-    shadowColor: '#000',
-    position: 'absolute',
+    paddingTop: "10%",
+    shadowColor: "#000",
+    position: "absolute",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -458,54 +462,54 @@ const styles = StyleSheet.create({
   },
 
   modalTitle: {
-    fontFamily: 'Montserrat_700Bold',
-    textAlign: 'center',
-    color: 'black',
-    fontWeight: 'bold',
+    fontFamily: "Montserrat_700Bold",
+    textAlign: "center",
+    color: "black",
+    fontWeight: "bold",
     fontSize: 25,
   },
 
   modalText: {
-    fontFamily: 'Roboto_400Regular',
-    alignSelf: 'center',
-    color: 'black',
+    fontFamily: "Roboto_400Regular",
+    alignSelf: "center",
+    color: "black",
   },
 
   modalButtons: {
-    position: 'absolute',
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: '15%',
-    bottom: '5%',
+    position: "absolute",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: "15%",
+    bottom: "5%",
   },
 
   modalButton: {
     borderWidth: 2,
-    borderColor: 'black',
-    width: '35%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "black",
+    width: "35%",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 5,
   },
 
   search: {
-    alignSelf: 'center',
+    alignSelf: "center",
     borderRadius: 15,
-    width: '90%',
+    width: "90%",
     height: 35,
     marginVertical: 10,
   },
 
   inputStyle: {
     fontSize: 15,
-    fontFamily: 'Roboto_400Regular',
+    fontFamily: "Roboto_400Regular",
   },
 
   listItem: {
     paddingLeft: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
   },
 
   names: {
@@ -514,7 +518,7 @@ const styles = StyleSheet.create({
   },
 
   addButton: {
-    position: 'absolute',
+    position: "absolute",
     right: 15,
   },
 });
